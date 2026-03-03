ff167d2f9a6e97f056a52f35518e8baf5d7d83bf
retroretroretro4242-code <retro.retro.retro.42.42@gmail.com>
2026-03-03 14:20:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True  # Üye join eventleri için gerekli
